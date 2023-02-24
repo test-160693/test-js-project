f4f8e2074befa18d6f9ffef01ae6a12f579ee5e5
test-160693 <126173395+test-160693@users.noreply.github.com>
2023-02-24 17:35:14 +0530

    Update welcome.js

--- welcome.js
+++ welcome.js
@@ -1,5 +1,41 @@
-function print() {
-  var i = 0;
-  console.log("Printing");
-  console.log("Printing");
-}
+function fire_open_api_submit() {
+
+        var search = {}
+        search["data"] = $("#text").val();
+        //search["email"] = $("#email").val();
+        search["targetLng"] = $("#countryID1").val();
+
+        console.log("data === "+$("#text").val());
+
+        $("#btn-search").prop("disabled", true);
+
+        $.ajax({
+            type: "POST",
+            contentType: "application/json",
+            url: "/api/v1/open/translate",
+            data: JSON.stringify(search),
+            dataType: 'json',
+            cache: false,
+            timeout: 600000,
+            success: function (data) {
+                var json = "<h4>Ajax Response</h4><pre>"
+                    + JSON.stringify(data, null, 4) + "</pre>";
+                var translatedText = data.data;
+                var detectedLng = data.detectedSourceLanguage;
+                console.log("data === "+translatedText);
+                $('#translatedText').html(translatedText);
+                console.log("SUCCESS : ", data);
+                $("#translatedText").prop("disabled", true);
+
+            },
+            error: function (e) {
+
+                var json = "<h4>Ajax Response</h4><pre>"
+                    + e.responseText + "</pre>";
+                $('#feedback').html(json);
+
+                console.log("ERROR : ", e);
+                $("#translatedText").prop("disabled", true);
+
+            }
+        });
